c7fdd2c10f513ca90e2afcf7664b49796fabeb4f
stephen cyron <stephen.cyron@fdmgroup.com>
2022-01-19 16:09:21 -0500

    Switched to requests.Session() to handle cookies

--- scrape.py
+++ scrape.py
@@ -5,13 +5,19 @@ from info import headers
 
 def scrape_ups(tracking_number):
 
-    data = '{"Locale":"en_US","TrackingNumber":[' + str(tracking_number) + ']}'
+    s = requests.Session()
+    s.get('https://www.ups.com/track')
 
-    response = requests.post('https://www.ups.com/track/api/Track/GetStatus', headers=headers, data=data)
+    headers['x-xsrf-token'] = s.cookies.get('X-XSRF-TOKEN-ST')
+
+    data = '{"Locale":"en_US","TrackingNumber":[' + str(tracking_number) + ']}'
+    response = s.post('https://www.ups.com/track/api/Track/GetStatus', headers=headers, data=data)
+
     if (response.status_code != 200):
         return "Error"
+
     status = json.loads(response.text)
     if 'statusText' in status:
-        status = status.get('statusText')
-
-    return (status)
+        return(status.get('statusText'))
+    else:
+        return "Error"
